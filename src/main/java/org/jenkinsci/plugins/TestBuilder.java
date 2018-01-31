5e7229d6d04191325cf240becfd48ecdf80bfe78
Deyan Kalinov <deyan.kalinov@progress.com>
2018-01-31 11:04:56 +0200

    Plugin now can be started on slave node

--- src/main/java/org/jenkinsci/plugins/TestBuilder.java
+++ src/main/java/org/jenkinsci/plugins/TestBuilder.java
@@ -7,20 +7,19 @@ import hudson.model.AbstractProject;
 import hudson.model.Result;
 import hudson.model.Run;
 import hudson.model.TaskListener;
+import hudson.remoting.Callable;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.Builder;
 import hudson.util.FormValidation;
 import jenkins.tasks.SimpleBuildStep;
+import org.jenkinsci.remoting.RoleChecker;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
 import org.kohsuke.stapler.QueryParameter;
 
 import javax.annotation.Nonnull;
 import javax.servlet.ServletException;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -31,14 +30,14 @@ import static org.jenkinsci.plugins.Utils.fileExists;
 import static org.jenkinsci.plugins.Utils.isEmpty;
 
 @SuppressWarnings("unused")
-public class TestBuilder extends Builder implements SimpleBuildStep {
+public class TestBuilder extends Builder implements SimpleBuildStep, Serializable {
 
+    private static final long serialVersionUID = 82342459898912L;
 
     private String apiRunnerPath;
     private String project;
     private String test;
     private String startFrom;
-    private TestType testType = TestType.SINGLE_TEST;
     private String stopAfter;
     private String variable;
     private boolean dontSaveContexts;
@@ -67,155 +66,198 @@ public class TestBuilder extends Builder implements SimpleBuildStep {
 
     @Override
     public void perform(@Nonnull Run<?, ?> run, @Nonnull FilePath workspace, @Nonnull Launcher launcher, @Nonnull TaskListener listener) throws InterruptedException, IOException {
-        if (System.getProperty("os.name").toLowerCase().contains("windows")){
-            this.isWindows = true;
-        }
 
-        String workspacePath = run.getEnvironment(listener).get("WORKSPACE", null);
-        String outputFileName = "ApiResults-" + System.currentTimeMillis() + ".xml";
-        String command = buildCommand(workspacePath, outputFileName);
-        listener.getLogger().println("command: " + command);
-        prepareWorkspace(workspacePath);
-        Runtime rt = Runtime.getRuntime();
-        Process proc = rt.exec(command);
+        MyCallable task = new MyCallable(String.valueOf(workspace),
+                Constants.API_STUDIO_RESULTS_DIR,
+                this.apiRunnerPath,
+                this.testAsUnit,
+                this.project,
+                this.test,
+                this.dontSaveContexts,
+                variable);
 
-        BufferedReader stdInput = new BufferedReader(new
-                InputStreamReader(proc.getInputStream()));
-
-        BufferedReader stdError = new BufferedReader(new
-                InputStreamReader(proc.getErrorStream()));
-
-        listener.getLogger().println("Command output:\n");
-        String s = null;
-        while ((s = stdInput.readLine()) != null) {
-            listener.getLogger().println(s);
-        }
-
-        listener.getLogger().println("STD Error output (if any):\n");
-        while ((s = stdError.readLine()) != null) {
-            listener.error(s);
-        }
-
-        String fullOutputName = workspace + File.separator + Constants.API_STUDIO_RESULTS_DIR + File.separator + outputFileName ;
-        if (!fileExists(fullOutputName)) {
-            listener.error("Result file doesn't exists: " + fullOutputName);
+        String result = launcher.getChannel().call(task);
+        listener.getLogger().println(result);
+        if (result != null && result.contains(" Finished.") && result.contains("[INFO]  Project compile completed.")){
+            run.setResult(Result.SUCCESS);
+        } else {
             run.setResult(Result.FAILURE);
         }
     }
 
-    private void prepareWorkspace(String workspace) {
-        File index = new File(workspace + File.separator + Constants.API_STUDIO_RESULTS_DIR);
-        if (!index.exists()) {
-            index.mkdir();
-        } else {
-            String[] entries = index.list();
-            for (String s : entries) {
-                File currentFile = new File(index.getPath(), s);
-                currentFile.delete();
-            }
-            if (!index.exists()) {
-                index.mkdir();
-            }
+
+    class MyCallable implements Callable<String, IOException> {
+        private static final long serialVersionUID = 439832542381L;
+
+        private String workspace;
+        private String resultsDir;
+        private String apiRunnerPath;
+        private String project;
+        private String test;
+        private String variable;
+
+        private boolean testAsUnit;
+        private boolean isWindows;
+        private boolean dontSaveContexts;
+
+        public MyCallable(String workspace,
+                          String resultsDir,
+                          String apiRunnerPath,
+                          boolean testAsUnit,
+                          String project,
+                          String test,
+                          boolean dontSaveContexts,
+                          String variable){
+            this.workspace = workspace;
+            this.resultsDir = resultsDir;
+            this.testAsUnit = testAsUnit;
+            this.apiRunnerPath = apiRunnerPath;
+            this.project = project;
+            this.test = test;
+            this.dontSaveContexts = dontSaveContexts;
+            this.variable = variable;
         }
-    }
 
-    private String buildCommand(String workspace, String outputFileName) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(this.normalizeExecutable(this.apiRunnerPath));
+        @Override
+        public void checkRoles(RoleChecker roleChecker) throws SecurityException {
 
-        sb.append(" test ");
-        if (!isEmpty(this.project)) {
-            sb.append("-p \"");
-            sb.append(normalizePath(workspace, this.project));
-            sb.append("\"");
         }
 
-        if (!isEmpty(this.test)) {
-            String[] tests = this.test.split("\n");
-            for (int idx = 0; idx < tests.length; idx++){
-                if (!isEmpty(tests[idx])) {
-                    if (tests[idx].startsWith("." + File.separator)) {
-                        sb.append(" -t \"");
-                    } else {
-                        sb.append(" -t \"." + File.separator);
+        @Override
+        public String call() throws IOException {
+
+            String output = "\nRunning OS: Linux\n";
+
+            if (System.getProperty("os.name").toLowerCase().contains("windows")){
+                this.isWindows = true;
+                output = "\nRunning OS: Windows\n";
+            }
+
+            String outputFileName = "ApiResults-" + System.currentTimeMillis() + ".xml";
+            String command = buildCommand(this.workspace, outputFileName);
+            output += "\nCommand: \n" + command + "\n";
+
+            Runtime rt = Runtime.getRuntime();
+            Process proc = rt.exec(command);
+
+            BufferedReader stdInput = new BufferedReader(new
+                    InputStreamReader(proc.getInputStream()));
+
+            BufferedReader stdError = new BufferedReader(new
+                    InputStreamReader(proc.getErrorStream()));
+
+            output += "\nCommand output:\n";
+            String s = null;
+            while ((s = stdInput.readLine()) != null) {
+                output += s + "\n";
+            }
+
+            output += "\nSTD Error output (if any):\n";
+            while ((s = stdError.readLine()) != null) {
+                output += s + "\n";
+            }
+
+            output +="\nCommand exit code: " + proc.exitValue() +" \n";
+            return output;
+        }
+        private String buildCommand(String workspace, String outputFileName) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("\"" + this.normalizeExecutable(this.apiRunnerPath) + "\"");
+
+            sb.append(" test ");
+            if (!isEmpty(this.project)) {
+                sb.append("-p \"");
+                sb.append(normalizePath(workspace, this.project));
+                sb.append("\"");
+            }
+
+            if (!isEmpty(this.test)) {
+                String[] tests = this.test.split("\n");
+                for (int idx = 0; idx < tests.length; idx++){
+                    if (!isEmpty(tests[idx])) {
+                        if (tests[idx].startsWith("." + File.separator)) {
+                            sb.append(" -t \"");
+                        } else {
+                            sb.append(" -t \"." + File.separator);
+                        }
+                        sb.append(tests[idx]);
+                        sb.append("\"");
                     }
-                    sb.append(tests[idx]);
-                    sb.append("\"");
                 }
             }
-        }
 
-        sb.append(" ");
-        sb.append("-o \"");
-        sb.append(normalizePath(workspace, Constants.API_STUDIO_RESULTS_DIR + File.separator + outputFileName));
-        sb.append("\"");
+            sb.append(" ");
+            sb.append("-o \"");
+            sb.append(normalizePath(workspace, Constants.API_STUDIO_RESULTS_DIR + File.separator + outputFileName));
+            sb.append("\"");
 
-        if (!this.dontSaveContexts) {
-            sb.append(" --save-contexts");
-        }
-
-        sb.append(" --verbose-compile");
-        sb.append(" -f ");
-        if (this.testAsUnit) {
-            sb.append("junit");
-        } else {
-            sb.append("junitsteps");
-        }
-
-        if (!isEmpty(this.variable)) {
-            String[] vars = this.variable.split("\n");
-            String prefix = " ";
-            for (int idx = 0; idx < vars.length; idx++){
-                sb.append(prefix);
-                sb.append("-v ");
-                sb.append(vars[idx]);
-                sb.append(" ");
-                prefix = "";
+            if (!this.dontSaveContexts) {
+                sb.append(" --save-contexts");
             }
+
+            sb.append(" --verbose-compile");
+            sb.append(" -f ");
+            if (this.testAsUnit) {
+                sb.append("junit");
+            } else {
+                sb.append("junitsteps");
+            }
+
+            if (!isEmpty(this.variable)) {
+                String[] vars = this.variable.split("\n");
+                String prefix = " ";
+                for (int idx = 0; idx < vars.length; idx++){
+                    sb.append(prefix);
+                    sb.append("-v ");
+                    sb.append(vars[idx]);
+                    sb.append(" ");
+                    prefix = "";
+                }
+            }
+
+            return sb.toString();
         }
 
-        return sb.toString();
-    }
+        private String normalizeExecutable(String apiStudioRunnerPath) {
+            String pathToLowerCase = apiStudioRunnerPath.toLowerCase();
+            String command = "";
+            if (!isWindows){
+                command = "mono ";
+            }
+            if (pathToLowerCase.endsWith(File.separator)) {
+                return command + apiStudioRunnerPath + TELERIK_API_TESTING_RUNNER_EXE;
+            } else if (!pathToLowerCase.endsWith(TELERIK_API_TESTING_RUNNER_EXE.toLowerCase())) {
+                return command + apiStudioRunnerPath + "\\" + TELERIK_API_TESTING_RUNNER_EXE;
+            }
+            return command + apiStudioRunnerPath;
 
-    private String normalizeExecutable(String apiStudioRunnerPath) {
-        String pathToLowerCase = apiStudioRunnerPath.toLowerCase();
-        String command = "";
-        if (!isWindows){
-            command = "mono ";
         }
-        if (pathToLowerCase.endsWith(File.separator)) {
-            return command + apiStudioRunnerPath + TELERIK_API_TESTING_RUNNER_EXE;
-        } else if (!pathToLowerCase.endsWith(TELERIK_API_TESTING_RUNNER_EXE.toLowerCase())) {
-            return command + apiStudioRunnerPath + "\\" + TELERIK_API_TESTING_RUNNER_EXE;
-        }
-        return command + apiStudioRunnerPath;
 
-    }
-
-    private String normalizePath(String workspace, String path){
-        String result;
-        if (isWindows) {
-            Matcher m = Pattern.compile("^\\D:\\\\.*$").matcher(path);
-            if (!m.find()) {
+        private String normalizePath(String workspace, String path){
+            String result;
+            if (isWindows) {
+                Matcher m = Pattern.compile("^\\D:\\\\.*$").matcher(path);
+                if (!m.find()) {
+                    if (path.startsWith(File.separator)) {
+                        result = workspace + path;
+                    } else {
+                        result = workspace + File.separator + path;
+                    }
+                } else {
+                    result = path;
+                }
+                if (result.endsWith(File.separator)) {
+                    result = result.substring(0, result.length() - 1);
+                }
+            } else {
                 if (path.startsWith(File.separator)) {
-                    result = workspace + path;
+                    result = path;
                 } else {
                     result = workspace + File.separator + path;
                 }
-            } else {
-                result = path;
             }
-            if (result.endsWith(File.separator)) {
-                result = result.substring(0, result.length() - 1);
-            }
-        } else {
-            if (path.startsWith(File.separator)) {
-                result = path;
-            } else {
-                result = workspace + File.separator + path;
-            }
+            return result;
         }
-        return result;
     }
 
     @SuppressWarnings("unused")
